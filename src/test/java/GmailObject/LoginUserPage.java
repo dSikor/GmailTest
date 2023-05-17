c3df3c85e22c4e95c42a244edbbb1842bd266954
dSikor <dsikora08@gmail.com>
2023-05-17 19:38:38 +0200

    add userEmailOrMobilePhone WebElement

--- src/test/java/GmailObject/LoginUserPage.java
+++ src/test/java/GmailObject/LoginUserPage.java
@@ -1,7 +1,26 @@
 package GmailObject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginUserPage {
+    WebDriver cnap_driver;
 
+    public LoginUserPage(WebDriver mdriver)
+    {
+        this.cnap_driver=mdriver;
+        PageFactory.initElements(mdriver,this);
+    }
 
+    @FindBy(name="identifier")
+    WebElement userEmailOrMobilePhone;
 
+    public void userEmailOrMobilePhone(String emailOrPhone) {
+        userEmailOrMobilePhone.sendKeys(emailOrPhone);
+    }
+    public WebElement userEmailOrMobilePhone() {
+        return userEmailOrMobilePhone;
+    }
 }
