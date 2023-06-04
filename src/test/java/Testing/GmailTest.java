9ac06260f5aa1e076cfa477ee9d189189162938d
dSikor <dsikora08@gmail.com>
2023-06-04 16:20:17 +0200

    add user agent in ChromeOptions

--- src/test/java/Testing/GmailTest.java
+++ src/test/java/Testing/GmailTest.java
@@ -21,6 +21,7 @@ public class GmailTest {
         System.setProperty("webdriver.chrome.driver", driverPath);
         options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36");
         driver=new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get(homePageAddress);
